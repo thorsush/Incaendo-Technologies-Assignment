4c22874420e2244b0c1907230a7fb001750a81df
thorsush <thoratsushil703.com>
2022-01-19 15:28:13 +0530

    added form submit feature

--- src/Components/SignInCard.js
+++ src/Components/SignInCard.js
@@ -6,34 +6,36 @@ export default class SignInCard extends Component {
     return (
       <div className="sign_in_card">
         <div className="form">
-          <div className="first_two">
-            <TextField
-              className="text_field"
-              id="outlined-basic"
-              label="Nome"
-              variant="outlined"
-            />
-            <TextField
-              className="text_field"
-              id="outlined-basic"
-              label="Cognome"
-              variant="outlined"
-            />
-          </div>
-          <TextField
-            className="text_field email"
-            id="outlined-basic"
-            label="email"
-            variant="outlined"
-          />
-          <div className="codice">
-            <div className="coupun">Codice Coupun</div>
-            <div className="user">
-            <div className="user_image"></div>
-            <div className="user_data">Nessun coupon inserito</div>
+          <form action="submit">
+            <div className="first_two">
+              <TextField
+                className="text_field"
+                id="outlined-basic"
+                label="Nome"
+                variant="outlined"
+              />
+              <TextField
+                className="text_field"
+                id="outlined-basic"
+                label="Cognome"
+                variant="outlined"
+              />
             </div>
-          </div>
-          <button>Attiva</button>
+            <TextField
+              className="text_field email"
+              id="outlined-basic"
+              label="email"
+              variant="outlined"
+            />
+            <div className="codice">
+              <div className="coupun">Codice Coupun</div>
+              <div className="user">
+                <div className="user_image"></div>
+                <div className="user_data">Nessun coupon inserito</div>
+              </div>
+            </div>
+            <button onClick="submit">Attiva</button>
+          </form>
         </div>
       </div>
     );
